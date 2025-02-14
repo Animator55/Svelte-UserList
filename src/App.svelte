c2698dae77a95d0bbc1b7129d46e01044f3810c4
Animator55 <animator55developer@gmail.com>
2025-02-14 20:42:59 -0300

    removed prev

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,59 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import UserList from "./lib/UserList.svelte";
+  import Login from "./lib/Login.svelte";
+  import UserView from "./lib/UserView.svelte";
+
+  let currentRoute = window.location.pathname
+
+  const toast = (val: {
+    title: string;
+    content: string;
+    icon: string;
+    _id: string;
+  }) => {
+    let container = document.querySelector(".toast-container");
+    if (!container) return;
+
+    const colorSelector: { [key: string]: string } = {
+      warn: "var(--corange)",
+      xmark: "var(--cred)",
+      check: "var(--cgreen)",
+    };
+    const iconSelector: { [key: string]: string } = {
+      warn: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="triangle-exclamation" class="svg-inline--fa fa-triangle-exclamation " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>',
+      xmark:
+        '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-xmark" class="svg-inline--fa fa-circle-xmark " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"></path></svg>',
+      check:
+        '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-check" class="svg-inline--fa fa-circle-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"></path></svg>',
+    };
+
+    let toast = document.createElement("section");
+    toast.innerHTML = `<header style="color: ${colorSelector[val.icon]};">
+          ${iconSelector[val.icon]}
+          <h3>${val.title}</h3>
+          </header>
+      <p>${val.content}</p>`;
+    toast.className = "toast";
+    container.appendChild(toast);
+    setTimeout(() => {
+      if (toast) toast.remove();
+    }, 3300);
+  };
+  let routeComponent;
+
+  console.log(currentRoute)
+
+  $: {
+    if (currentRoute === "/") routeComponent = UserList;
+    else if (currentRoute === "/auth") routeComponent = Login;
+    else if (currentRoute === "/user") routeComponent = UserView;
+    else routeComponent = null;
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<div class="toast-container"></div>
+<svelte:component this={routeComponent} {toast} />
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  @import "./assets/App.css";
 </style>
